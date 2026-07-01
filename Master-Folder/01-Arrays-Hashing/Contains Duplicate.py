fb57d09a12b0114cf3d2be7e4d1fa58678d9b22a
CreaBiza <hasitagarwal@gmail.com>
2026-07-01 17:32:45 +0530

    Delete Master-Folder/01-Arrays-Hashing/Contains Duplicate.py

--- Master-Folder/01-Arrays-Hashing/Contains Duplicate.py
+++
@@ -1,7 +0,0 @@
-def containsDuplicate(self, nums: List[int]) -> bool:
-    seen = set()
-    for i in range(len(nums)):
-        if nums[i] in seen:
-            return True
-        seen.add(nums[i])
-    return False

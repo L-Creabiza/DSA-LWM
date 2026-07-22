53cfcb9e807d01ee702a9bb14a37eb57b724a929
CreaBiza <hasitagarwal@gmail.com>
2026-07-22 10:30:24 +0530

    added in the wrong dir

--- Master-Folder/NeetCode150/01-Arrays-Hashing/125-valid-palindrome/valid-palindrome.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        drizzy = "".join(char for char in s if char.isalnum()).lower()
-        if drizzy == drizzy[::-1]:
-            return True
-        else:
-            return False
